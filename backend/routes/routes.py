cec076d0f55108da46a2e590513a77b7e4ab2ac9
kunnger <Kunnger40543@gmail.com>
2024-06-12 19:04:17 +0900

    prediction part

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, render_template, request
 from backend import app
-from ..controller import embed
+from ..controller import embed, predict_final
 
 
 @app.route("/")
@@ -15,4 +15,14 @@ def embed_route():
     algorithm = data["algorithm"]
     print(f"Ontology: {ontology}, Algorithm: {algorithm}")
     result = embed.embed_func(ontology_file=ontology, algorithm=algorithm)
+    return jsonify({"message": result})
+
+
+@app.route("/predict", methods=["POST"])
+def predict_route():
+    data = request.json
+    ontology = data["ontology"]
+    algorithm = data["algorithm"]
+    print(f"Ontology: {ontology}, Algorithm: {algorithm}")
+    result = predict_final.predict_func(ontology_file=ontology, algorithm=algorithm)
     return jsonify({"message": result})
